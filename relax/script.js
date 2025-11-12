eb03d4fe91fb8150f65e5d80a193e970ce93242a
ah1874-dot <ah1874@scarletmail.rutgers.edu>
2025-11-12 09:14:59 -0500

    Update script.js

--- relax/script.js
+++ relax/script.js
@@ -9,6 +9,7 @@ function changeStyle() {
     border-radius: 8px;
   `;
 
+ 
   setTimeout(() => {
     textElement.style.transition = "opacity 1.5s ease";
     textElement.style.opacity = "0";
@@ -19,3 +20,24 @@ function changeStyle() {
   }, 10000);
 }
 
+
+let button = document.querySelector("#myButton");
+let vantaEffect = null;
+
+button.addEventListener("click", function() {
+  if (!vantaEffect) {
+    vantaEffect = VANTA.CLOUDS2({
+      el: "#vanta-bg",
+      mouseControls: true,
+      touchControls: true,
+      gyroControls: false,
+      minHeight: 200.00,
+      minWidth: 200.00,
+      scale: 1.00,
+      backgroundAlpha: 0.0,
+      skyColor: 0x5070ff,
+      cloudColor: 0xffffff,
+      texturePath: "https://raw.githubusercontent.com/tengbao/vanta/master/src/images/noise.png"
+    });
+  }
+});
