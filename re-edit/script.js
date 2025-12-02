097981bf93a0175d8c62b8b9d8b9e0c291e760d7
ah1874-dot <ah1874@scarletmail.rutgers.edu>
2025-12-02 11:40:59 -0500

    Update script.js

--- re-edit/script.js
+++ re-edit/script.js
@@ -1,34 +1,33 @@
 const imageData = [
   {
-
-    img: "img1.jpg",
-    imgHover: "img1a.jpg",
+    img: "https://upload.wikimedia.org/wikipedia/commons/8/8c/David_-_The_Death_of_Socrates.jpg",
+    imgHover: "https://upload.wikimedia.org/wikipedia/commons/8/8c/David_-_The_Death_of_Socrates.jpg",
     text: "Master, what makes us human?",
     textHover: "Selecting all images with traffic lights."
   },
   {
-    img: "img2.jpg",
-    imgHover: "img2a.jpg",
+    img: "https://upload.wikimedia.org/wikipedia/commons/f/fc/Edward_Portielje_-_The_winning_Hand.jpg",
+    imgHover: "https://upload.wikimedia.org/wikipedia/commons/f/fc/Edward_Portielje_-_The_winning_Hand.jpg",
     text: "What is your favorite party trick?",
     textHover: "Not going."
   },
   {
-    img: "img3.jpg",
-    imgHover: "img3a.jpg",
+    img: "https://media.mutualart.com/Images/2022_11/30/10/100323224/edward-cucuel-les-yachts-70NI2-338.Jpeg",
+    imgHover: "https://media.mutualart.com/Images/2022_11/30/10/100323224/edward-cucuel-les-yachts-70NI2-338.Jpeg",
     text: "You’ve been underperforming in work lately,",
     textHover: "I’m just matching your underappreciation."
   },
   {
-    img: "img4.jpg",
-    imgHover: "img4a.jpg",
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/Interior_with_a_Sleeping_Man_and_a_Woman_Darning_Socks_by_Wybrand_Hendriks.jpg/500px-Interior_with_a_Sleeping_Man_and_a_Woman_Darning_Socks_by_Wybrand_Hendriks.jpg",
+    imgHover: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/Interior_with_a_Sleeping_Man_and_a_Woman_Darning_Socks_by_Wybrand_Hendriks.jpg/500px-Interior_with_a_Sleeping_Man_and_a_Woman_Darning_Socks_by_Wybrand_Hendriks.jpg",
     text: "I thought you said you were interesting.",
     textHover: "I said I was into resting."
   },
   {
-    img: "img5.jpg",
-    imgHover: "img5a.jpg",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1ZisKTWu5TG0rS3mgCWKDM5Hwcqz_mVL1og&s",
+    imgHover: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1ZisKTWu5TG0rS3mgCWKDM5Hwcqz_mVL1og&s",
     text: "I’m lost without you.",
-    textHover: "Use Google Maps."
+    textHover: "Use google maps."
   }
 ];
 
@@ -41,10 +40,10 @@ imageData.forEach(item => {
   container.innerHTML = `
     <img src="${item.img}" class="img-base">
     <img src="${item.imgHover}" class="img-hover">
+
     <div class="img-text">${item.text}</div>
     <div class="img-text img-text-hover">${item.textHover}</div>
   `;
 
   grid.appendChild(container);
 });
-
