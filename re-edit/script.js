450ac0d3afa706f733f0e90e67b5663195184f32
ah1874-dot <ah1874@scarletmail.rutgers.edu>
2025-12-02 11:56:44 -0500

    Update script.js

--- re-edit/script.js
+++ re-edit/script.js
@@ -1,16 +1,33 @@
-// Add as many items as you want here
 const items = [
   {
-    base: "img/photo1.jpg",
-    hover: "img/photo1-hover.jpg",
-    text: "Base Text Here",
-    textHover: "Hover Text Here"
+    base: "img/img1.jpg",
+    hover: "img/img1a.jpg",
+    text: "Master, what makes us human?",
+    textHover: "Selecting all images with traffic lights."
   },
   {
-    base: "img/photo2.jpg",
-    hover: "img/photo2-hover.jpg",
-    text: "Another Base Text",
-    textHover: "Another Hover Text"
+    base: "img/img2.jpg",
+    hover: "img/img2a.jpg",
+    text: "What is your favorite party trick?",
+    textHover: "Not going."
+  },
+  {
+    base: "img/img3.jpg",
+    hover: "img/img3a.jpg",
+    text: "You’ve been underperforming in work lately,",
+    textHover: "I’m just matching your underappreciation."
+  },
+  {
+    base: "img/img4.jpg",
+    hover: "img/img4a.jpg",
+    text: "I though you said you were interesting.",
+    textHover: "I said I was into resting."
+  },
+  {
+    base: "img/img5.jpg",
+    hover: "img/img5a.jpg",
+    text: "I’m lost without you.",
+    textHover: "Use google maps."
   }
 ];
 
